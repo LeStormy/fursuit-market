8cd3178adf4f56fb11005ea86ccbaa259129d3ba
stormy <raphael.beltrand@gmail.com>
2018-07-19 21:27:56 +0200

    home page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    @tmp = "tmp"
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module FursuitMarket
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # assets pipeline config to work on Heroku
+    config.assets.initialize_on_precompile = false
+
+    # add active admin assets to precompile list, loaded from vendor/assets
+    config.assets.precompile += %w( active_admin.js active_admin.css.scss )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
